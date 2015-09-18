b7c9b76a250da9ea7277ea0eceb8ff92cc3722b2
lukhar <lukasz.har@gmail.com>
2015-09-18 15:32:17 +0100

    Simplified startup file.

--- startup.sh
+++ startup.sh
@@ -1,25 +1,19 @@
 #!/bin/sh
 
-git --version 2>&1 >/dev/null 
+git --version 2>&1 >/dev/null
 GIT_IS_AVAILABLE=$?
 
-if [ -d bundle ]; then
-    echo "It appears you already have a bundle dir delete it before proceding.\n"
-    return -1
-fi
-
-if [ -d swp ]; then
-    echo "It appears you already have a swp dir delete it before proceding.\n"
-    return -1
-fi
-
-mkdir bundle
-
-mkdir swp
-
-if [ ! $GIT_IS_AVAILABLE -eq 0 ]; then 
+if [ ! $GIT_IS_AVAILABLE -eq 0 ]; then
     echo "You need to install git first!\n"
     return -1
+fi
+
+if [ ! -d bundle ]; then
+    mkdir bundle
+fi
+
+if [ ! -d swp ]; then
+    mkdir swp
 fi
 
 git clone git://github.com/Shougo/neobundle.vim bundle/neobundle.vim
